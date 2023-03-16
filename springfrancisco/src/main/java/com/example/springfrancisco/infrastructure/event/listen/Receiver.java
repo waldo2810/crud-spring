457adb5b43d1536dec9d40e0f4339613f7af63c3
Waldo Sobrino <waldo2810@gmail.com>
2023-03-16 16:24:03 -0500

    configure DELETE with rabbitmq

--- springfrancisco/src/main/java/com/example/springfrancisco/infrastructure/event/listen/Receiver.java
+++ springfrancisco/src/main/java/com/example/springfrancisco/infrastructure/event/listen/Receiver.java
@@ -1,15 +1,29 @@
 package com.example.springfrancisco.infrastructure.event.listen;
 
+import com.example.springfrancisco.application.car.CarDelete;
+import com.example.springfrancisco.domain.entities.Carro;
+import com.example.springfrancisco.infrastructure.api.controller.CarController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
 @Slf4j
 public class Receiver {
   public static String RECEIVE_METHOD_NAME = "receiveMessage";
+  private final ObjectMapper objectMapper;
+  private final CarController carController;
 
-  public void receiveMessage(byte[] message){
-    log.info("Received message [{}]",new String(message, StandardCharsets.UTF_8));
+
+  public void receiveMessage(byte[] message) throws JsonProcessingException {
+    String messageConvert = new String(message, StandardCharsets.UTF_8);
+    Carro carro = objectMapper.readValue(messageConvert,Carro.class);
+    log.info("Received message [{}]",carro);
+
+    carController.deleteCar(carro.getPlaca());
   }
 }
